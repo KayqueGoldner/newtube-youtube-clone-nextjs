9b1200f0934d1bac110295a122e71a5892b8146e
Kayque Goldner <kayquegoldner@gmail.com>
2025-02-11 22:53:11 -0300

    trpc setup

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,3 +1,19 @@
-export default function Home() {
-  return <div>List videos</div>;
+import { Suspense } from "react";
+import { HydrateClient, trpc } from "@/trpc/server";
+import { ErrorBoundary } from "react-error-boundary";
+
+import { PageClient } from "./client";
+
+export default async function Home() {
+  void trpc.hello.prefetch({ text: "Kayque" });
+
+  return (
+    <HydrateClient>
+      <Suspense fallback={<p>Loading...</p>}>
+        <ErrorBoundary fallback={<p>Error...</p>}>
+          <PageClient />
+        </ErrorBoundary>
+      </Suspense>
+    </HydrateClient>
+  );
 }
