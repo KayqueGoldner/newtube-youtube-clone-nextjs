dc472c0a42cde062c8984eafa3b032193b6ebc97
Kayque Goldner <kayquegoldner@gmail.com>
2025-03-09 15:46:53 -0300

    video page

---
+++ src/modules/videos/ui/views/video-view.tsx
@@ -0,0 +1,26 @@
+import { CommentsSection } from "@/modules/videos/ui/sections/comments-section";
+import { SuggestionsSection } from "@/modules/videos/ui/sections/suggestions-section";
+import { VideoSection } from "@/modules/videos/ui/sections/video-section";
+
+interface VideoViewProps {
+  videoId: string;
+}
+
+export const VideoView = ({ videoId }: VideoViewProps) => {
+  return (
+    <div className="mx-auto mb-10 flex max-w-[1700px] flex-col px-4 pt-2.5">
+      <div className="flex flex-col gap-6 xl:flex-row">
+        <div className="min-w-0 flex-1">
+          <VideoSection videoId={videoId} />
+          <div className="mt-4 block xl:hidden">
+            <SuggestionsSection />
+          </div>
+          <CommentsSection />
+        </div>
+        <div className="hidden w-full shrink xl:block xl:w-[380px] 2xl:w-[460px]">
+          <SuggestionsSection />
+        </div>
+      </div>
+    </div>
+  );
+};
